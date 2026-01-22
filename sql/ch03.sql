39b380e481a3dec1800e211ed5d25905fe4d8242
yuri7534-7734 <yuri7534@naver.com>
2026-01-22 18:31:35 +0900

    1월 22일 수업

--- sql/ch03.sql
+++ sql/ch03.sql
@@ -194,6 +194,10 @@ end as 결과;
 -- 1. 고객회사명2 : 기존 고객회사명 중 앞의 두 자리를 *로 변환한다.
 -- 2. 전화번호2 : 기존 전화번호의 (xxx)xxx-xxxx 형식을 xxx-xxx-xxxx형식으로 변환한다.
 
+SELECT concat(repeat('*',2),(SUBSTR(고객회사명, 3))) as 고객회사명2
+      ,replace(replace(전화번호,'(',''),')','-') as 전화번호2
+FROM 고객
+
 -- 2. 다음 조건에 따라 주문 세부 테이블의 모든 컬럼과 주문금액, 할인금액, 실제 주문금액을 보이시오. 
 -- 이때 모든 금액은 1의 단위에서 버림을 하고 10원 단위까지 보이시오.
 -- 조건
@@ -201,20 +205,6 @@ end as 결과;
 -- 2. 할인금액 : 주문수량 * 단가 * 할인율
 -- 3. 실주문금액 : 주문금액 - 할인금액
 
--- 3. 사원 테이블에서 전체 사원의 이름, 생일, 만나이, 입사일, 입사일수, 
--- 입사한 지 500일 후의 날짜를 보이시오.
-
--- 4. 고객 테이블에서 도시 컬럼의 데이터를 다음 조건에 따라 ‘대도시’와 ‘도시’로 구분하고, 
--- 마일리지 점수에 따라서 ‘VVIP’, ‘VIP’, ‘일반 고객’으로 구분하시오.
--- 조건
--- 1. 도시 구분: ‘특별시’나 ‘광역시’는 ‘대도시’로, 그 나머지 도시는 ‘도시’로 구분한다.
--- 2. 마일리지 구분 : 마일리지가 100,000점 이상이면 ‘VVIP고객’, 10,000점 이상이면 ‘VIP고객’, 그 나머지는 ‘일반고객’으로 구분한다
-
-
-
-SELECT concat(repeat('*',2),(SUBSTR(고객회사명, 3))) as 고객회사명2
-      ,replace(replace(전화번호,'(',''),')','-') as 전화번호2
-FROM 고객
 
 SELECT 주문번호, 제품번호, TRUNCATE(단가,-1) as 단가, 주문수량, 할인율
 ,TRUNCATE(주문수량 * 단가,-1) as 주문금액
@@ -222,42 +212,139 @@ SELECT 주문번호, 제품번호, TRUNCATE(단가,-1) as 단가, 주문수량, 
 ,TRUNCATE((주문수량*단가)-(주문수량*단가*할인율),-1) as 실주문금액
 FROM 주문세부
 
--- CTE(WITH절) : 모든 DBMS에서 사용가능, 단 mysql 5.x대 사용불가
-WITH 주문금액 AS(
-     SELECT *, (주문수량*단가) AS 주문금액
-     FROM 주문세부
-),
-할인금액 AS(
-SELECT *, TRUNCATE(주문수량 * 단가 * 할인율, -1) AS 할인금액
-FROM 주문금액
+
+-- CTE(WITH절)
+WITH 주문cte AS (
+    SELECT 주문수량
+        ,단가
+        ,할인율
+        ,주문수량 * 단가 AS '주문금액'
+        ,주문수량 * 단가 * 할인율 AS '할인금액'
+    FROM 주문세부
 )
-SELECT *, (주문금액 - 할인금액) AS 실주문금액
-FROM 할인금액;
-)
+SELECT 주문금액
+    ,FLOOR(할인금액) AS 할인금액
+    ,주문금액 - TRUNCATE(할인금액, -1) AS '실제 주문금액'
+FROM 주문cte;
 
 
-SELECT 이름, 생일
-, TIMESTAMPDIFF(year, 생일, now())as 만나이, 입사일
-, datediff(now(),입사일)as 입사일수
-, adddate(입사일,interval 50 day)as '입사+500'
+-- 서브쿼리(Subquery) 방법
+SELECT T.*
+     , (T.주문금액 - T.할인금액) AS 실주문금액
+FROM (
+    -- 안쪽에서 주문금액과 할인금액을 먼저 계산
+    SELECT *
+         , (주문수량 * 단가) AS 주문금액
+         , TRUNCATE(주문수량 * 단가 * 할인율, -1) AS 할인금액
+    FROM 주문세부
+) AS T;
+-- T는 서브쿼리의 임시 결과물을 의미함.
+
+-- 3. 사원 테이블에서 전체 사원의 이름, 생일, 만나이, 입사일, 입사일수, 
+-- 입사한 지 500일 후의 날짜를 보이시오.
+
+SELECT 이름
+       , 생일
+       , TIMESTAMPDIFF(year, 생일, CURDATE())as 만나이
+       , 입사일
+       , datediff(CURDATE(),입사일)as 입사일수
+       , adddate(입사일,interval 500 day)as '입사+500'
 from 사원
 
-SELECT 도시,
-   CASE 
-	WHEN 도시 like '%광역시' OR 도시 LIKE '%특별시' THEN '대도시'
-ELSE '도시'
-	END AS '도시 구분'
+
+-- 4. 고객 테이블에서 도시 컬럼의 데이터를 다음 조건에 따라 ‘대도시’와 ‘도시’로 구분하고, 
+-- 마일리지 점수에 따라서 ‘VVIP’, ‘VIP’, ‘일반 고객’으로 구분하시오.
+-- 조건
+-- 1. 도시 구분: ‘특별시’나 ‘광역시’는 ‘대도시’로, 그 나머지 도시는 ‘도시’로 구분한다.
+-- 2. 마일리지 구분 : 마일리지가 100,000점 이상이면 ‘VVIP고객’, 10,000점 이상이면 ‘VIP고객’, 그 나머지는 ‘일반고객’으로 구분한다
+
+SELECT 도시
+     -- 삼항연산자처럼 동작
+    ,IF(도시 LIKE '%특별시' OR 도시 LIKE '%광역', '대도시','도시') AS 도시구분
+     ,마일리지
+     ,CASE WHEN 마일리지 >= 100000 THEN 'VVIP고객'
+      WHEN 마일리지 >= 10000 THEN 'VIP고객'
+      ELSE '일반고객'
+      END AS '마일리지 구분'
 FROM 고객
 
+
+
 SELECT 도시,
-   CASE 
-	WHEN 도시 like '%광역시' OR 도시 LIKE '%특별시' THEN '대도시'
-ELSE '도시'
+    CASE 
+	WHEN 도시 LIKE '%광역시'OR 도시 LIKE '%특별시' THEN '대도시'
+    ELSE '도시'
 	END AS '도시 구분'
+	FROM 고객;
+	
+SELECT 마일리지, 
+	CASE WHEN 마일리지 >= 100000 THEN 'VVIP고객'
+    WHEN 마일리지 >= 10000 THEN 'VIP고객'
+    ELSE '일반고객'
+	END AS '마일리지 구분'
+    FROM 고객;
+
+
+SELECT COUNT(*)
+FROM 주문
+
+-- 5. 주문 테이블에서 주문번호, 고객번호, 주문일 및 주문년도, 분기
+--    월, 일, 요일, 한글요일을 보이시오.
+
+SELECT 주문번호, 고객번호
+,주문일
+,year(주문일)AS 주문년도
+,QUARTER(주문일) AS 분기
+, CASE
+	WHEN MONTH(주문일) BETWEEN 1 AND 3 THEN '1분기'
+	WHEN MONTH(주문일) BETWEEN 4 AND 6 THEN '2분기'
+	WHEN MONTH(주문일) BETWEEN 7 AND 9 THEN '3분기'
+	ELSE '4분기'
+END AS '분기'
+, MONTH(주문일) AS 월
+, DAY(주문일) AS 일
+, CASE WEEKDAY(주문일) -- 단순형
+	WHEN 0 THEN '월요일'
+	WHEN 1 THEN '화요일'
+	WHEN 2 THEN '수요일'
+	WHEN 3 THEN '목요일'
+	WHEN 4 THEN '금요일'
+	ELSE '주말'
+END AS '한글요일'
+FROM 주문
+
+
+-- 6. 주문 테이블에서 요청일보다 발송일이 7일 이상 늦은 주문내역을 보이시오.
+SELECT *
+FROM 주문
+WHERE DATEDIFF(요청일,발송일) <= 7 
+
+-- 실전문제
+-- 1. 고객테이블에서 이름에 '정'이 들어가는 담당자명을 검색하시오.
+
+SELECT 담당자명
 FROM 고객
+WHERE 담당자명 LIKE '%정%'
 
 
+-- 2. 제품테이블에서 제품번호, 제품명, 재고, 재고구분을 보이시오.
+--    -재고구분 : 재고가 100개 이상이면 ‘과다재고’, 10개 이상이면 ‘적정’, 
+--     나머지는 ‘재고부족’
 
+SELECT 제품번호, 제품명, 재고
+      , CASE -- 범위형
+      	   WHEN 재고 >= 100 THEN '과다재고'
+      	   WHEN 재고 >= 10 THEN '적정'
+      	   ELSE '재고부족'
+      END AS '재고구분'
+FROM 제품
+
+-- 3. 사원테이블에서 입사한 지 40개월이 지난 사원을 찾아,
+--    이름, 부서번호, 직위, 입사일, 입사일수, 입사개월수를 출력하시오.
+
+SELECT 이름, 부서번호, 직위, 입사일, DATEDIFF(NOW(), 입사일)AS 입사일수, TIMESTAMPDIFF(MONTH, 입사일, NOW() )AS 입사개월수
+FROM 사원
+WHERE TIMESTAMPDIFF(MONTH, 입사일,NOW()) > 40;
 
 
 -- CTRL + SHIFT + F ( 수동 포맷팅 )
---
+++ sql/ch04.sql
@@ -0,0 +1,113 @@
+-- ch04.sql
+
+-- 집계함수는 null값은 연산에서 제외한다.
+SELECT count(고객번호), count(도시), count(지역)
+FROM 고객;
+
+SELECT sum(마일리지), avg(마일리지), MIN(마일리지), MAX(마일리지)
+      ,TRUNCATE( STDDEV(마일리지),-1 )AS 표준편차
+FROM 고객
+
+SELECT sum(마일리지), avg(마일리지), MIN(마일리지), MAX(마일리지)
+      ,TRUNCATE( STDDEV(마일리지),-1 )AS 표준편차
+FROM 고객
+WHERE 도시 = '서울특별시';
+
+-- GROUP BY절 : 특정컬럼에 대해 그룹(묶어서)으로 집계할 때
+
+-- SELECT 옆에 컬럼이름은 (집계함수제외) 반드시 GROUP BY절에 써야 됨.
+SELECT 도시
+       , COUNT(*) AS '도시별 고객수'
+       , AVG(마일리지) AS '도시별 평균마일리지'
+FROM 고객
+GROUP BY 1, 2; -- 중복제거+그룹핑.
+
+SELECT 담당자직위 , 도시
+     , COUNT(*)AS '고객수'
+     , AVG(마일리지)AS '도시별 평균 마일리지'
+FROM 고객
+GROUP BY 1, 2
+ORDER BY 1, 2
+
+
+
+-- COUNT()함수에 DISTINCT 예약어를 추가
+-- COUNT(DISTINCT 도시) :  중복값을 한 번씩만 센다.
+SELECT 도시, COUNT(도시), COUNT(DISTINCT 도시)
+FROM 고객
+GROUP BY 도시;
+
+SELECT COUNT(도시) AS 전체데이터수 -- 93(전체고객수)
+     , COUNT(DISTINCT 도시) AS 거래도시수 -- 27
+FROM 고객;
+
+-- 주문년도별로 집계를 해보자
+SELECT YEAR(주문일) AS 주문년도
+     , COUNT(*) AS 주문건수
+FROM 주문
+GROUP BY YEAR(주문일);
+
+-- 분기별, 소계(ROLLUP)를 집계 해보자.
+SELECT YEAR(주문일) AS 주문년도
+     ,QUARTER(주문일) AS 분기
+     ,COUNT(*) AS 주문건수
+FROM 주문
+GROUP BY YEAR(주문일),QUARTER(주문일)
+WITH ROLLUP; -- 분류별 소계, 총계를 내는 주문
+
+-- 주문 테이블에서 요청일보다 발송이 늦어진 주문을
+-- 월별로 집계(요약)해 보자.
+SELECT MONTH(주문일) AS 주문월
+     , COUNT(*)AS 주문건수
+FROM 주문
+WHERE 요청일 < 발송일
+GROUP BY MONTH(주문일)
+ORDER BY MONTH(주문일) ASC;
+
+-- 제품 테이블에서 제품명에 '아이스크림'이 들어간 제품들의 재고합을
+-- 집계하여 출력하시오.
+SELECT 제품명, sum(재고) AS 재고합
+FROM 제품
+WHERE 제품명 LIKE '%아이스크림%'
+GROUP BY 제품명
+WITH ROLLUP;
+
+-- 실전문제
+-- 1. 주문세부 테이블에서 주문수량합과 주문금액합을 보이시오.
+SELECT sum(주문수량) AS 주문수량합
+     , TRUNCATE(sum(단가 * 주문수량*(1-할인율)),0) AS 주문금액합
+FROM 주문세부;
+
+-- 2. 주문세부 테이블에서 주문번호별로 주문된 제품번호의 목록과 
+--    주문금액합을 보이시오.
+--  주문번호는 주문 건당 하나씩 발급됨.
+
+SELECT 주문번호
+     , 제품번호
+     , sum(단가 * 주문수량) AS 주문금액합
+FROM 주문세부
+GROUP BY 주문번호, 제품번호;
+
+-- 3. 주문 테이블에서 2021년 주문내역에 대해서 고객번호별로 
+-- 주문건수를 보이되, 주문건수가 많은 상위 3건의 고객의 정보만 보이시오.
+
+SELECT 고객번호, count(*) AS 주문건수
+FROM 주문
+WHERE year(주문일)= '2021'
+GROUP BY 1
+ORDER BY 주문건수 desc
+LIMIT 3;
+
+
+-- GROUP_CONCAT()함수 : 여러 행의 문자열을 결행해 줌.
+SELECT 직위, GROUP_CONCAT(이름 SEPARATOR ', ')
+FROM 사원
+GROUP BY 직위
+ORDER BY 직위;
+
+SELECT 직위, COUNT(직위), 이름
+FROM 사원
+GROUP BY 직위, 이름
+ORDER BY 직위;
+
+
