06f78ad1affd3094c671c87fe1ec0722563f6e31
yuri7534 <yuri7534@naver.com>
2026-02-24 00:40:24 +0900

    in home

--- springboot/Ex09Model/src/main/java/com/study/Ex09Model/Ex09ModelApplication.java
+++ springboot/Ex09Model/src/main/java/com/study/Ex09Model/Ex09ModelApplication.java
@@ -3,9 +3,12 @@ package com.study.Ex09Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//스프링부트 실행 시작점_1
 @SpringBootApplication
 public class Ex09ModelApplication {
-
+//main() 실행 -> 내장 톰캣 실행_2
+//스프링 컨테이너 생성_3
+//Controller, Bean 자동 스캔
 	public static void main(String[] args) {
 		SpringApplication.run(Ex09ModelApplication.class, args);
 	}
